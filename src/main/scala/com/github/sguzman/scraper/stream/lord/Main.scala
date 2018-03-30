b5a8668715b77c4b83f6eab06e6a4009c2b72a20
Salvador Guzman <guzmansalv@gmail.com>
2018-03-30 10:16:30 -0700

    first attempt...

--- src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
@@ -1,11 +1,19 @@
 package com.github.sguzman.scraper.stream.lord
 
 import java.io.{File, FileInputStream, FileOutputStream}
+import java.net.SocketTimeoutException
 
 import http.HttpCache
+import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{elementList, element}
+import scalaj.http.Http
+
+import scala.language.reflectiveCalls
+import scala.util.{Failure, Success}
 
 object Main{
-  val httpCache = {
+  var httpCache = {
     val file = new File("./http.data")
     if (!file.exists) file.createNewFile()
     val in = new FileInputStream("./http.data")
@@ -21,7 +29,7 @@ object Main{
     out.close()
   }
 
-  val itemCache = {
+  var itemCache = {
     val file = new File("./item.data")
     if (!file.exists) file.createNewFile()
     val in = new FileInputStream("./item.data")
@@ -44,7 +52,54 @@ object Main{
     }
   })
 
+  def retryHttpGet(url: String): String = util.Try(Http(url).asString) match {
+    case Success(v) => v.body
+    case Failure(e) => e match {
+      case _: SocketTimeoutException => retryHttpGet(url)
+      case _ => throw new Exception(s"Url: $url; ${e.getMessage}")
+    }
+  }
+
+  implicit final class DocWrap(doc: Browser#DocumentType) {
+    def map(s: String) = doc.>?>(element(s)) match {
+      case Some(v) => v
+      case None => throw new Exception(s)
+    }
+
+    def flatMap(s: String) = doc.>?>(elementList(s)) match {
+      case Some(v) => v
+      case None => throw new Exception(s)
+    }
+  }
+
+  trait Cacheable[B] {
+    def contains(s: String): Boolean
+    def apply(s: String): B
+    def put(s: String, b: B): Unit
+  }
+
+  def get[A <: Cacheable[B], B](url: String, cache: A) (f: Browser#DocumentType => B): B =
+    if (cache.contains(url)) cache(url)
+    else if (httpCache.cache.contains(url)) {
+      scribe.info(s"Missed item cache for $url")
+      val html = httpCache.cache(url)
+      val result = f(JsoupBrowser().parseString(html))
+      cache.put(url, result)
+      get(url, cache)(f)
+    } else {
+      scribe.debug(s"Missed http cache... calling $url")
+      val html = retryHttpGet(url)
+      httpCache = httpCache.addCache((url, html))
+      get(url, cache)(f)
+    }
+
   def main(args: Array[String]): Unit = {
-    println("Hello")
+    val seed = "https://www.animebam.net/"
+    val items = "div.container > div.row > div.col-md-6 > div.panel.panel-default > div.panel-footer > ul.series_alpha > li > a[href]"
+    get(seed, new Cacheable[Seq[String]] {
+      override def contains(s: String) = itemCache.links.nonEmpty
+      override def apply(s: String) = itemCache.links
+      override def put(s: String, b: Seq[String]): Unit = itemCache = itemCache.addAllLinks(b)
+    }) (_.flatMap(items))
   }
 }
