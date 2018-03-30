afbdb540a25040049a806e9bb9d028be53fc5843
Salvador Guzman <guzmansalv@gmail.com>
2018-03-30 09:21:49 -0700

    finished cache mechanism

--- src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
@@ -1,6 +1,6 @@
 package com.github.sguzman.scraper.stream.lord
 
-import java.io.{File, FileInputStream}
+import java.io.{File, FileInputStream, FileOutputStream}
 
 import http.HttpCache
 
@@ -9,8 +9,40 @@ object Main{
     val file = new File("./http.data")
     if (!file.exists) file.createNewFile()
     val in = new FileInputStream("./http.data")
-    HttpCache.parseFrom(in)
+    val cache = HttpCache.parseFrom(in)
+    in.close()
+    cache
   }
+
+  def writeHttp = {
+    val file = new File("./http.data")
+    val out = new FileOutputStream(file)
+    httpCache.writeTo(out)
+    out.close()
+  }
+
+  val itemCache = {
+    val file = new File("./item.data")
+    if (!file.exists) file.createNewFile()
+    val in = new FileInputStream("./item.data")
+    val cache = items.Items.parseFrom(in)
+    in.close()
+    cache
+  }
+
+  def writeItem = {
+    val file = new File("./http.data")
+    val out = new FileOutputStream(file)
+    itemCache.writeTo(out)
+    out.close()
+  }
+
+  Runtime.getRuntime.addShutdownHook(new Thread {
+    override def run(): Unit = {
+      writeItem
+      writeHttp
+    }
+  })
 
   def main(args: Array[String]): Unit = {
     println("Hello")
