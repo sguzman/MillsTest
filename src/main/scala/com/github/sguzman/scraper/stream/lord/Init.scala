69a51abe013e76f4d34b919c0f2f8cb9202dd3ef
Salvador Guzman <guzmansalv@gmail.com>
2018-03-29 22:22:58 -0700

    use lambdas

--- src/main/scala/com/github/sguzman/scraper/stream/lord/Init.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Init.scala
@@ -5,6 +5,9 @@ import java.net.SocketTimeoutException
 import io.circe.parser.decode
 import io.circe.syntax._
 import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.element
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.elementList
 import scalaj.http.Http
 
 import scala.collection.mutable
@@ -51,11 +54,14 @@ object Init {
     }
   })
 
-  def getItemCache[A](key: String, dec: String => A): A = {
-    dec(cache(key))
+  def getItemCache[A](key: String, dec: String => Either[io.circe.Error, A]): A = {
+    dec(cache(key)) match {
+      case Left(e) => throw new Exception(e.getMessage)
+      case Right(v) => v
+    }
   }
 
-  def getHttpCache[A](key: String, proc: Browser#DocumentType => String, dec: String => A, newKey: String => Unit = println): A = {
+  def getHttpCache[A](key: String, proc: Browser#DocumentType => String, dec: String => Either[io.circe.Error, A], newKey: String => Unit = println): A = {
     val doc = JsoupBrowser().parseString(httpCache(key))
 
     if (!cache.contains(key)) {
@@ -67,7 +73,7 @@ object Init {
     getItemCache(key, dec)
   }
 
-  def get[A](url: String, proc: Browser#DocumentType => String, dec: String => A, newKey: String => Unit = println): A = {
+  def get[A](url: String, proc: Browser#DocumentType => String, dec: String => Either[io.circe.Error, A], newKey: String => Unit = println): A = {
     def retry: String = Try(Http(url).asString) match {
       case Success(v) => v.body
       case Failure(e) => e match {
@@ -79,11 +85,16 @@ object Init {
     getHttpCache(url, proc, dec)
   }
 
-  def cascade[A](url: String, proc: Browser#DocumentType => String, dec: String => A, newKey: String => Unit = println): A = {
+  def cascade[A](url: String, proc: Browser#DocumentType => String, dec: String => Either[io.circe.Error, A], newKey: String => Unit = println): A = {
     if (cache.contains(url)) getItemCache(url, dec)
     else if (httpCache.contains(url)) getHttpCache(url, proc, dec)
     else get(url, proc, dec)
   }
 
   def removeHttp(s: List[String]): Unit = s.foreach(httpCache.remove)
+
+  implicit final class DocWrap(doc: Browser#DocumentType) {
+    def Map(s: String) = doc.>>(element(s))
+    def FlatMap(s: String) = doc.>>(elementList(s))
+  }
 }
--- src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
@@ -1,25 +1,32 @@
 package com.github.sguzman.scraper.stream.lord
 
+import com.github.sguzman.scraper.stream.lord.Init.DocWrap
+import io.circe.generic.auto._
 import io.circe.parser.decode
 import io.circe.syntax._
-import io.circe.generic.auto._
-import net.ruippeixotog.scalascraper.browser.Browser
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.scraper.ContentExtractors.elementList
 
 object Main {
   def main(args: Array[String]): Unit = {
     final case class Anime(title: String, link: String)
-
     val url = "https://ww4.animejolt.com/anime/"
-    def proc(doc: Browser#DocumentType): String = {
+
+    val anime = Init.cascade(url, {doc =>
       doc.>>(elementList(".list-group-item > div > a[href]"))
         .map(a => Anime(a.innerHtml, a.attr("href")))
         .asJson.spaces4
+    }, s => decode[List[Anime]](s))
+
+    final case class AnimePage(img: String, p: String, eps: List[String])
+    anime.map{a =>
+      Init.cascade(a.link, {doc =>
+        AnimePage(
+          doc.Map("div.series-container group > img[src]").attr("src"),
+          doc.Map("div.series-container group > p:first_child").text,
+          doc.FlatMap("li.list-group-item > div > a[href]").map(_.attr("href"))
+        ).asJson.spaces4
+      }, s => decode[AnimePage](s))
     }
-
-    def dec(s: String) = decode[List[Anime]](s).right.get
-
-    Init.cascade(url, proc, dec)
   }
 }
