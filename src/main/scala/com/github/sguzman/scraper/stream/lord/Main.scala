83d73821fceec7849e4a27d27a0dd1cac80d62a8
Salvador Guzman <guzmansalv@gmail.com>
2018-03-29 22:34:03 -0700

    add some error handling

--- src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
@@ -4,8 +4,8 @@ import com.github.sguzman.scraper.stream.lord.Init.DocWrap
 import io.circe.generic.auto._
 import io.circe.parser.decode
 import io.circe.syntax._
-import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.scraper.ContentExtractors.elementList
+
+import scala.util.{Failure, Success}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -13,20 +13,25 @@ object Main {
     val url = "https://ww4.animejolt.com/anime/"
 
     val anime = Init.cascade(url, {doc =>
-      doc.>>(elementList(".list-group-item > div > a[href]"))
+      doc.FlatMap(".list-group-item > div > a[href]")
         .map(a => Anime(a.innerHtml, a.attr("href")))
         .asJson.spaces4
     }, s => decode[List[Anime]](s))
 
     final case class AnimePage(img: String, p: String, eps: List[String])
     anime.map{a =>
-      Init.cascade(a.link, {doc =>
-        AnimePage(
-          doc.Map("div.series-container group > img[src]").attr("src"),
-          doc.Map("div.series-container group > p:first_child").text,
-          doc.FlatMap("li.list-group-item > div > a[href]").map(_.attr("href"))
-        ).asJson.spaces4
-      }, s => decode[AnimePage](s))
+      util.Try{
+        Init.cascade(a.link, {doc =>
+          AnimePage(
+            doc.Map("div.series-container.group > img[src]").attr("src"),
+            doc.Map("div.series-container.group > p").text,
+            doc.FlatMap("li.list-group-item > div > a[href]").map(_.attr("href"))
+          ).asJson.spaces4
+        }, s => decode[AnimePage](s))
+      } match {
+        case Success(v) => v
+        case Failure(e) => throw new Exception(s"$a; ${e.getMessage}")
+      }
     }
   }
 }
