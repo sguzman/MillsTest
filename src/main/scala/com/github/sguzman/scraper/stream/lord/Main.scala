3b80bc64afe109f77db8ddfb224943b1d0b2eef3
Salvador Guzman <guzmansalv@gmail.com>
2018-03-30 08:21:06 -0700

    change name of message

--- src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/stream/lord/Main.scala
@@ -1,6 +1,17 @@
 package com.github.sguzman.scraper.stream.lord
 
+import java.io.{File, FileInputStream}
+
+import http.HttpCache
+
 object Main{
+  val httpCache = {
+    val file = new File("./http.data")
+    if (!file.exists) file.createNewFile()
+    val in = new FileInputStream("./http.data")
+    HttpCache.parseFrom(in)
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello")
   }
